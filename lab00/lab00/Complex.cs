0f18b7fee9483333d4e4ed0a35f706ee91a65425
laurentiustamate94 <laurentiu.stamate@equilobe.com>
2015-03-24 23:30:06 +0200

    Added lab00 - Introducere si Relaxare

---
+++ lab00/lab00/Complex.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace lab00
+{
+    class Complex
+    {
+        public float real;
+        public float im;
+
+        public static List<Complex> ReadData(string filename)
+        {
+            var result = new List<Complex>();
+
+            try
+            {
+                var lines = File.ReadAllLines(filename);
+
+                var n = int.Parse(lines[0]);
+
+                for (int i = 1; i < n + 1; i++)
+                {
+                    var current = new Complex();
+
+                    current.real = float.Parse(lines[i].Split(' ')[0]);
+                    current.im = float.Parse(lines[i].Split(' ')[1]);
+
+                    result.Add(current);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Fisierul de date nu este in regula !!");
+            }
+
+            return result;
+        }
+
+        public static void WriteContents(List<Complex> data)
+        {
+            foreach (Complex number in data)
+                Console.WriteLine(number);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0}, {1})", real, im);
+        }
+    }
+}
---
+++ lab00/lab00/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace lab00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Citire date din fisier */
+            var numbers = Complex.ReadData("Resources/date.in");
+            Console.WriteLine("Vectorul initial:");
+            Complex.WriteContents(numbers);
+
+            /* Verifica sortarea. */
+            var sorted = GetSorted(numbers);
+            Console.WriteLine("Vectorul sortat:");
+            Complex.WriteContents(sorted);
+
+            /* Verifica maparea. */
+            var mapping = GetMapping(numbers);
+            Console.WriteLine("Maparea:");
+
+            foreach (var element in sorted)
+                Console.WriteLine("{0} e pe pozitia {1}",
+                    element, mapping.FirstOrDefault(x => x.Key.Equals(element)).Value);
+        }
+
+        static List<Complex> GetSorted(List<Complex> numbers)
+        {
+            var result = new List<Complex>();
+
+            /* TODO 
+             * Folosind SortedSet, adaugati elementele din numbers in ordine crescatoare.
+             * Pentru SortedSet folositi comparatorul definit mai jos 
+             */
+
+            return result;
+        }
+
+        static Dictionary<Complex, int> GetMapping(List<Complex> numbers)
+        {
+            var result = new Dictionary<Complex, int>();
+
+            /* TODO 
+             * Adaugati in map, pentru fiecare element din numbers, 
+             * pozitia sa in vectorul sortat.
+             */
+
+            return result;
+        }
+    }
+
+    class ComplexComparator : Comparer<Complex>
+    {
+        public override int Compare(Complex x, Complex y)
+        {
+            // TODO Intoarceti un numar pozitiv daca a > b, negativ altfel
+
+            throw new NotImplementedException();
+        }
+    }
+}
